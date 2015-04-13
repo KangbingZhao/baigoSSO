09180cb1a16680ee1f5983e8b394ecb268aafd9b
KevinZhao <zhaokangbing@hotmail.com>
2015-04-13 10:15:15 +0800

    inital commit from ali

--- config/opt_base.inc.php
+++ config/opt_base.inc.php
@@ -1,11 +1,11 @@
 <?php
 define("BG_SITE_NAME", "baigo SSO");
-define("BG_SITE_DOMAIN", "www.baigo.net");
-define("BG_SITE_URL", "http://www.baigo.net");
+define("BG_SITE_DOMAIN", "121.41.82.206/baigosso");
+define("BG_SITE_URL", "http://121.41.82.206/baigosso");
 define("BG_SITE_PERPAGE", 30);
 define("BG_SITE_TIMEZONE", "Etc/GMT+8");
 define("BG_SITE_DATE", "Y-m-d");
 define("BG_SITE_DATESHORT", "m-d");
 define("BG_SITE_TIME", "H:i:s");
 define("BG_SITE_TIMESHORT", "H:i");
-define("BG_SITE_SSIN", "SeXqP8");
+define("BG_SITE_SSIN", "LWvfCk");
