dcec625bf09f81e5ac9793db3e6638c26d61b32a
Kangbing Zhao <zhaokangbing@hotmail.com>
2015-04-20 20:15:22 +0800

    修复了user-auth返回id为字符串

--- mypage/user_identification.php
+++ mypage/user_identification.php
@@ -32,7 +32,7 @@ if($_POST){
                     $return_data = json_encode(array("is_login"=>"true","u_id"=>$uid,"u_name"=>$u_name));
                 }else {//token已经过期,删除token并返回结果
                     mysql_query("DELETE FROM sso_token WHERE token='$token'");
-                    $return_data = json_encode(array("is_login"=>"false","u_id"=>-1,"u_name"=>"is_expired"));
+                    $return_data = json_encode(array("is_login"=>"false","u_id"=>"-1","u_name"=>"is_expired"));
 //                    print_r(mysql_fetch_array(mysql_query("SELECT FROM sso_token WHERE token = '$token'")));
                     if(mysql_affected_rows()<1)  {
 //                        die("删除过期token出错!");
@@ -40,12 +40,12 @@ if($_POST){
                     }
                 }
             }else {//查询不到对应的token，返回结果
-                $return_data = json_encode(array("is_login"=>"false","u_id"=>0,"u_name"=>"token_err"));
+                $return_data = json_encode(array("is_login"=>"false","u_id"=>"0","u_name"=>"token_err"));
             }
 
         } else {
 //            die("Access Deny!");
-            $return_data = json_encode(array("is_login"=>"deny","u_id"=>0,"u_name"=>"app_denied"));
+            $return_data = json_encode(array("is_login"=>"deny","u_id"=>"0","u_name"=>"app_denied"));
 
         }
         echo $return_data;
